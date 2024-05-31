c52e252b66e3c920f97f49c0e0e833587e39e774
IdoToxido <126401912+IdoToxido@users.noreply.github.com>
2024-05-31 12:33:50 +0300

    Create program.cs

---
+++ program.cs
@@ -0,0 +1,50 @@
+class Number(int Num, int SIndex, int EIndex)
+{
+    public int Num = Num;
+    public int SIndex = SIndex;
+    public int EIndex = EIndex;
+}
+class Program
+{
+    public static void Main()
+    {
+        string[] input = File.ReadAllLines("input.txt");
+    }
+    public static int[] FindValids(string[] Arr)
+    {
+        int[] RtnArr = [];
+        for (int i = 0; i < Arr.Length; i++)
+        {
+            if (i == 0)
+            {
+
+            }
+        }
+    }
+    public static Number GetNumber(string Enc)
+    {
+        Number rtn = new(-1, 0, -1);
+        string RtnStr = "";
+        for (int i = 0; i < Enc.Length; i++)
+        {
+            if (char.IsDigit(Enc[i]))
+            {
+                Console.WriteLine($"The char {Enc[i]} is a digit");
+                rtn.SIndex = i;
+                while (char.IsDigit(Enc[i]))
+                {
+                    RtnStr += Enc[i];
+                    i++;
+                }
+                rtn.EIndex = i;
+                rtn.Num = int.Parse(RtnStr);
+                return rtn;
+            }
+        }
+        return new(0, 0, 0);
+    }
+    public static bool IsSymbol(char C)
+    {
+        char[] chars = ['-','+','=','*','$'];
+    }
+}
